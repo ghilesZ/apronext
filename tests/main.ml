7cbde128115475a82ca3ac48115b972445126bf6
ghilesZ <ghiles.ziat@gmail.com>
2018-07-09 11:28:56 +0200

    fix bug bytes string introduced by 4.06.0. fix bug array_maker to lincons

--- tests/main.ml
+++ tests/main.ml
@@ -13,7 +13,7 @@ let test_pol env pts =
   let pol = Apol.of_generator_list env gens in
   let (l,h) = Apol.bound_variable_fs pol "z" in
   for i = iof l to iof h do
-    let cut = Apol.assign_fs pol "z" (foi i) in
+    let _cut = Apol.assign_fs pol "z" (foi i) in
     Graphics.clear_graph ();
     (* Apol.draw2d draw (a,b) cut; *)
     Unix.sleepf 0.01
@@ -24,7 +24,7 @@ let test_box env pts =
   let box = Abox.of_generator_list env gens in
   let (l,h) = Abox.bound_variable_fs box "z" in
   for i = iof l to iof h do
-    let cut = Abox.assign_fs box "z" (foi i) in
+    let _cut = Abox.assign_fs box "z" (foi i) in
     Graphics.clear_graph ();
     (* Abox.draw2d draw (a,b) cut; *)
     Unix.sleepf 0.01
@@ -35,7 +35,7 @@ let test_oct env pts =
   let oct = Aoct.of_generator_list env gens in
   let (l,h) = Aoct.bound_variable_fs oct "z" in
   for i = iof l to iof h do
-    let cut = Aoct.assign_fs oct "z" (foi i) in
+    let _cut = Aoct.assign_fs oct "z" (foi i) in
     Graphics.clear_graph ();
     (* Aoct.draw2d draw (a,b) cut; *)
     Unix.sleepf 0.01
@@ -46,7 +46,7 @@ let _ =
   Graphics.set_window_title "Apron domains";
   Graphics.loop_at_exit [] (fun _ -> ());
   Random.self_init();
-  let dim = 3 in
+  let _dim = 3 in
   (* let nb_point = 12 in *)
   (* let rand_point () = *)
   (*   iter (fun acc -> ((Random.float 400.) +. 200.)::acc ) [] dim *)
