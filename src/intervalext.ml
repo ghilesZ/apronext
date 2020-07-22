b75c439513c0b09b60a53d49874331339677e622
ghilesZ <ghiles.ziat@gmail.com>
2020-07-22 17:46:54 +0200

    code cln + add some features in interval

--- src/intervalext.ml
+++ src/intervalext.ml
@@ -12,8 +12,17 @@ let to_float a = (S.to_float a.inf),(S.to_float a.sup)
 
 let to_mpqf a = (S.to_mpqf a.inf),(S.to_mpqf a.sup)
 
+(** scalar range of an interval *)
 let range a = S.sub a.sup a.inf
 
+(** same as range but result as an mpqf *)
 let range_mpqf a = S.sub a.sup a.inf |> S.to_mpqf
 
+(** midpoint of an interval *)
 let mid a = S.add a.inf (S.div (S.sub a.sup a.inf) (S.of_int 2))
+
+(** Random uniform value within an interval, according to the type *)
+let spawn ({inf;sup}:t) =
+  let inf = S.to_mpqf inf and sup = S.to_mpqf sup in
+  let r = Mpqf.of_float (Random.float 1.) in
+  Mpqf.add inf (Mpqf.mul (Mpqf.sub sup inf) r)
