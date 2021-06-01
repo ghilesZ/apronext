dbfcfbfb3ab8dd55878bf64e959f6ceac80922ab
ziat <ghiles.ziat@gmail.com>
2021-06-01 11:26:55 +0200

    add typ info in domain

--- src/domain.ml
+++ src/domain.ml
@@ -32,7 +32,7 @@ module Make (D : ADomain) = struct
   module T = Tconsext
   module L = Linconsext
   module E = Environmentext
-  open A
+
 
   type t = D.t A.t
 
@@ -40,49 +40,49 @@ module Make (D : ADomain) = struct
 
   (** Set-theoritic operations *)
 
-  let bottom : Environmentext.t -> t = bottom man
+  let bottom : Environmentext.t -> t = A.bottom man
 
-  let top : Environmentext.t -> t = top man
+  let top : Environmentext.t -> t = A.top man
 
-  let join : t -> t -> t = join man
+  let join : t -> t -> t = A.join man
 
-  let meet : t -> t -> t = meet man
+  let meet : t -> t -> t = A.meet man
 
-  let widening : t -> t -> t = widening man
+  let widening : t -> t -> t = A.widening man
 
   (** predicates *)
 
-  let is_bottom : t -> bool = is_bottom man
+  let is_bottom : t -> bool = A.is_bottom man
 
-  let is_top : t -> bool = is_top man
+  let is_top : t -> bool = A.is_top man
 
-  let is_leq : t -> t -> bool = is_leq man
+  let is_leq : t -> t -> bool = A.is_leq man
 
-  let is_eq : t -> t -> bool = is_eq man
+  let is_eq : t -> t -> bool = A.is_eq man
 
   (** constraint satisfaction and filter *)
 
-  let sat_lincons : t -> Linconsext.t -> bool = sat_lincons man
+  let sat_lincons : t -> Linconsext.t -> bool = A.sat_lincons man
 
-  let sat_tcons : t -> Tconsext.t -> bool = sat_tcons man
+  let sat_tcons : t -> Tconsext.t -> bool = A.sat_tcons man
 
   let filter_lincons (abs:t) (l:Linconsext.t) : t =
     let ear = L.array_make abs.env 1 in
     L.array_set ear 0 l ;
-    meet_lincons_array man abs ear
+    A.meet_lincons_array man abs ear
 
   let filter_tcons (abs:t) (l:Tconsext.t) : t =
     let ear = T.array_make abs.env 1 in
     T.array_set ear 0 l ;
-    meet_tcons_array man abs ear
+    A.meet_tcons_array man abs ear
 
   (** of and to constraints/generator *)
 
-  let to_lincons_array : t -> Linconsext.earray = to_lincons_array man
+  let to_lincons_array : t -> Linconsext.earray = A.to_lincons_array man
 
-  let to_tcons_array : t -> Tconsext.earray = to_tcons_array man
+  let to_tcons_array : t -> Tconsext.earray = A.to_tcons_array man
 
-  let to_generator_array : t -> Generatorext.earray = to_generator_array man
+  let to_generator_array : t -> Generatorext.earray = A.to_generator_array man
 
   let to_lincons_list (abs:t) = abs |> to_lincons_array |> L.array_to_list
 
@@ -98,35 +98,35 @@ module Make (D : ADomain) = struct
       E.fold
         (fun acc var ->
           let c = Texprext.cst e (get_coeff v var) in
-          assign_texpr man acc var c None )
+          A.assign_texpr man acc var c None )
         (top e) e
     in
-    let closed = join_array man (Array.of_list (List.map ofvertice g)) in
-    Generatorext.array_of_list lray |> add_ray_array man closed
+    let closed = A.join_array man (Array.of_list (List.map ofvertice g)) in
+    Generatorext.array_of_list lray |> A.add_ray_array man closed
 
   let of_generator_array g : t = of_generator_list (G.array_to_list g)
 
-  let of_lincons_array : Environmentext.t -> Lincons1.earray -> t = of_lincons_array man
+  let of_lincons_array : Environmentext.t -> Lincons1.earray -> t = A.of_lincons_array man
 
-  let of_tcons_array : Environmentext.t -> Tcons1.earray -> t = of_tcons_array man
+  let of_tcons_array : Environmentext.t -> Tcons1.earray -> t = A.of_tcons_array man
 
   let of_lincons_list env l = of_lincons_array env (L.array_of_list l)
 
   let of_tcons_list env l = of_tcons_array env (T.array_of_list l)
 
-  let of_box : Environmentext.t -> Var.t array -> Intervalext.t array -> t = of_box man
+  let of_box : Environmentext.t -> Var.t array -> Intervalext.t array -> t = A.of_box man
 
   (** Environment and variable related operations *)
 
-  let get_environment : t -> Environmentext.t = env
+  let get_environment : t -> Environmentext.t = A.env
 
-  let change_environment (abs:t) e : t = change_environment man abs e false
-
-  (*FIXME : What is the purpose of None?*)
-  let assign_texpr (abs:t) var texpr : t = assign_texpr man abs var texpr None
+  let change_environment (abs:t) e : t = A.change_environment man abs e false
 
   (*FIXME : What is the purpose of None?*)
-  let assign_linexpr (abs:t) var linexpr : t = assign_linexpr man abs var linexpr None
+  let assign_texpr (abs:t) var texpr : t = A.assign_texpr man abs var texpr None
+
+  (*FIXME : What is the purpose of None?*)
+  let assign_linexpr (abs:t) var linexpr : t = A.assign_linexpr man abs var linexpr None
 
   let assign_f (abs:t) var f : t =
     let texpr = Texprext.cst_f f |> Texprext.of_expr abs.env in
@@ -136,7 +136,7 @@ module Make (D : ADomain) = struct
 
   (* utilities*)
   let add_var (abs:t) typ v : t =
-    let e = env abs in
+    let e = A.env abs in
     let ints, reals =
       if typ = Environment.INT then ([|v|], [||]) else ([||], [|v|])
     in
@@ -145,7 +145,7 @@ module Make (D : ADomain) = struct
 
   let add_var_s (abs:t) typ v : t = add_var abs typ (Var.of_string v)
 
-  let bound_variable (abs:t) v = bound_variable man abs v
+  let bound_variable (abs:t) v = A.bound_variable man abs v
 
   let bound_variable_f (abs:t) v =
     let open Intervalext in
@@ -160,7 +160,7 @@ module Make (D : ADomain) = struct
   let is_bounded_s (abs:t) v = is_bounded_variable abs (Var.of_string v)
 
   let is_bounded (abs:t) =
-    let env = env abs in
+    let env = A.env abs in
     try
       E.iter (fun v -> if is_bounded_variable abs v |> not then raise Exit) env ;
       true
@@ -168,16 +168,16 @@ module Make (D : ADomain) = struct
 
   (** Cross-domain conversion *)
   let to_box (abs:t) =
-    let env = env abs in
+    let env = A.env abs in
     let abs' = A.change_environment man abs env false in
     to_tcons_array abs' |> A.of_tcons_array (Box.manager_alloc ()) env
 
   let to_oct (abs:t) =
-    let env = env abs in
+    let env = A.env abs in
     to_lincons_array abs |> A.of_lincons_array (Oct.manager_alloc ()) env
 
   let to_poly (abs:t) =
-    let env = env abs in
+    let env = A.env abs in
     let abs' = A.change_environment man abs env false in
     to_tcons_array abs' |> A.of_tcons_array (Polka.manager_alloc_strict ()) env
 
@@ -196,7 +196,7 @@ module Make (D : ADomain) = struct
 
   (** Projection on 2 dimensions *)
   let proj2D (abs:t) v1 v2 : t =
-    let env = env abs in
+    let env = A.env abs in
     let add_var v res =
       if E.typ_of_var env v = E.INT then E.add_int v res else E.add_real v res
     in
@@ -207,7 +207,7 @@ module Make (D : ADomain) = struct
 
   (** Projection on 3 dimensions *)
   let proj3D (abs:t) v1 v2 v3 : t =
-    let env = env abs in
+    let env = A.env abs in
     let new_env = E.empty in
     let new_env =
       if Array.exists (( = ) v1) (E.get_ints env) then E.add_int v1 new_env
